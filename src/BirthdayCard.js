18ba97dc541847419ce98367589fedd7bfe16d14
Nathan Reidy <nathanjreidy@gmail.com>
2021-03-31 17:36:52 +1100

    Added props

--- src/BirthdayCard.js
+++ src/BirthdayCard.js
@@ -1,19 +1,17 @@
 import React from "react";
 
 const BirthdayCard = (props) => {
+  console.log(props);
+  const { id, name, age, img } = props;
   return (
     <div className="birthdayCard">
       <div className="imgContainer">
-        <img
-          src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg"
-          alt=""
-          className="img"
-        />
+        <img src={img} className="img" />
       </div>
 
       <div className="detailsContainer">
-        <p className="name">Bertie Yates</p>
-        <p className="age">29 years</p>
+        <p className="name">{name}</p>
+        <p className="age">{age}</p>
       </div>
     </div>
   );
