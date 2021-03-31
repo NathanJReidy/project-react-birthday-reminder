ec9ce7cf123f4524e22a12845adfa37b7b3172bf
Nathan Reidy <nathanjreidy@gmail.com>
2021-03-31 17:55:13 +1100

    Added state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
-import people from "./data";
+import data from "./data";
 import BirthdayCard from "./BirthdayCard";
+import React from "react";
 
 function App() {
+  const [people, setPeople] = React.useState(data);
+  const [birthdayCount, setBirthdayCount] = React.useState(5);
+
   return (
     <>
       <div className="centerCard">
-        <p>X birthdays today</p>
+        <p>{birthdayCount} birthdays today</p>
         <div className="birthdayCardList">
           {people.map((person) => {
             return <BirthdayCard key={person.id} {...person} />;
           })}
         </div>
-        <button className="btn">Clear All </button>
+        <button
+          className="btn"
+          onClick={() => {
+            setPeople([]);
+            setBirthdayCount(0);
+          }}
+        >
+          Clear All
+        </button>
       </div>
     </>
   );
